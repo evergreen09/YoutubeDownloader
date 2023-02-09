3090d3abc3f63d7ac342db1b90e471d27699dfe9
evergreen09 <j364254@gmail.com>
2023-02-09 14:44:49 -0600

    "Created a python program that crawls youtube links  on youtuber's video page then writes into a csv file."

--- webCrawler.py
+++ webCrawler.py
@@ -1,17 +1,31 @@
 import requests
+import re
 from selenium import webdriver
 import time
 from bs4 import BeautifulSoup
+import csv
+
+linkcsv = 'links.csv'
+linkarry = []
+utube = 'https://www.youtube.com'
+
 
 driver = webdriver.Chrome()
 url = 'https://www.youtube.com/@PaniBottle/videos'
-driver.maximize_window()
+
 driver.get(url)
 
 
-time.sleep(5)
+time.sleep(3)
 content = driver.page_source.encode('utf-8').strip()
 soup = BeautifulSoup(content,"html.parser")
 firstVid = soup.findAll("a", {"id": "video-title-link"})
+for link in firstVid:
+    for utlink in link:
+        linkarry.append([str(utube) + str(link.get('href'))])
+with open(linkcsv, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(linkarry)
+print(linkarry)
 
-print(firstVid)
+
